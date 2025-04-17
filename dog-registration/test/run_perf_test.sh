32be74846bfa246f127ac62de8cbabe4dffba5b2
Mesut Güneş <gunesmes@gmail.com>
2025-04-17 17:15:32 +0300

    fix ci.yml

--- dog-registration/test/run_perf_test.sh
+++ dog-registration/test/run_perf_test.sh
@@ -1,6 +1,6 @@
 for var in $(seq 1 100); do
     echo $var; 
-    k6 run dog-registration/test/performance_test_ml.js; 
+    k6 run performance_test_ml.js; 
     bash append_test_result.sh; 
     sleep 3;
 done    
